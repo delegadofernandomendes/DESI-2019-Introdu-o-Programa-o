6b7de0bd17731a7e24b0193a888da15fb8455da1
delegadofernandomendes <delegadofernandomendes@gmail.com>
2019-10-15 22:36:19 -0300

    Situação de Aprendizagem -  15 Out 2019

---
+++ sa/WebContent/js/cidade.js
@@ -0,0 +1,41 @@
+function carregarDadoscidade() {
+	// pegar a lista de estados do storage
+	
+	let listaEstadosStr = localStorage.getItem('listaEstado');
+	let listaEstados = [];
+	if (listaEstadosStr != null) {
+		listaEstados = JSON.parse(listaEstadosStr);
+	}
+	let comboEstado = document.getElementById("comboEstado");
+	let option;
+	for (let index = 0; index < listaEstados.length; index++) {
+		option = document.createElement("option");
+		option.text = listaEstados[index].nome;
+		option.value - listaEstados[index].nome;
+		comboEstado.add(option);
+	}
+}
+
+//construtor para Estado
+function Cidade(nome, estado){
+	this.nome = nome;
+	this.estado = estado;
+}
+
+function salvarCidade(){
+	//Pegar o elemento HTML com id "nome" e guardar em input
+	let input = document.getElementById('nome_cidade');
+	//Pegar o valor de dentro do input , ou seja, o que o usario digitou
+	// e guardar em nome (varial)
+	let nome = input.value;
+	// Salvar o nome digitado no storage com chave 'nome'
+	
+	let select = document.getElementById('comboEstado');
+	let estado = select.value;
+	
+	let cidade = new Cidade(nome, estado);
+	
+	let cidadeStr = JSON.stringify(cidade);
+	localStorage.setItem('Cidade', cidadeStr);
+
+}
--- sa/WebContent/js/sa.js
+++ sa/WebContent/js/sa.js
@@ -11,42 +11,37 @@ this.pais = pais;
 }
 
 function salvarEstado(){
-//Pegar o elemento HTML com id "nome" e guardar em input
-let input = document.getElementById('nome_estado');
-//Pegar o valor de dentro do input , ou seja, o que o usario digitou
-// e guardar em nome (varial)
-let nome = input.value;
-// Salvar o nome digitado no storage com chave 'nome'
-//Traz da página o nome do país
-input = document.getElementById('nome_pais')
-let pais = input.value;
-//usa o construtor Estado para criar o objeto estado
-let estado = new Estado(nome, pais);
-
-
-
-
-// pega o item do local storage
-let listEstadoStr = localStorage.getItem('listaEstado');
-//iniciliza a lista vazia
-let listEstado = [];
-
-
-//verificar se a string no local storage nao é null
-if(listEstadoStr !=null){
-listEstado = JSON.parse(listEstadoStr);
-}
-
-
-
-//adiciona um novo estado a lista de estados.
-listEstado.push(estado);
-
-listEstadoStr = JSON.stringify(listEstado);
-
-localStorage.setItem('listaEstado', listEstadoStr);
-
-//salva o objeto estado no local storage
-
+	//Pegar o elemento HTML com id "nome" e guardar em input
+	let input = document.getElementById('nome_estado');
+	//Pegar o valor de dentro do input , ou seja, o que o usario digitou
+	// e guardar em nome (varial)
+	let nome = input.value;
+	// Salvar o nome digitado no storage com chave 'nome'
+	//Traz da página o nome do país
+	input = document.getElementById('nome_pais')
+	let pais = input.value;
+	//usa o construtor Estado para criar o objeto estado
+	let estado = new Estado(nome, pais);
+	
+	
+	// pega o item do local storage
+	let listEstadoStr = localStorage.getItem('listaEstado');
+	//iniciliza a lista vazia
+	let listEstado = [];
+	
+	
+	//verificar se a string no local storage nao é null
+	if(listEstadoStr !=null){
+		listEstado = JSON.parse(listEstadoStr);
+	}
+	
+	//adiciona um novo estado a lista de estados.
+	listEstado.push(estado);
+	
+	listEstadoStr = JSON.stringify(listEstado);
+	
+	localStorage.setItem('listaEstado', listEstadoStr);
+	
+	//salva o objeto estado no local storage
 
 }
