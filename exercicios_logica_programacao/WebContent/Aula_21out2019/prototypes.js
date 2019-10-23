95d5aec79113e89101f2e27faa547c2cae1be1ae
delegadofernandomendes <delegadofernandomendes@gmail.com>
2019-10-22 21:21:06 -0300

    Aula 22 Out 2019

--- exercicios_logica_programacao/WebContent/Aula_21out2019/prototypes.js
+++ exercicios_logica_programacao/WebContent/Aula_21out2019/prototypes.js
@@ -1,15 +1,17 @@
 // Criação do construtor Produto
-function produto (nome, marca, preco, icms) {
+function Produto (nome, marca, preco) {
     this.nome = nome;
     this.marca = marca;
     this.preco = preco;
-    this.icms = icms;
 }
 
-produto.prototype.icms = 0;
+// Criação de um atributo do objeto.
+Produto.prototype.icms = 0;
 
 
-function calcularIcms () {
-    this.icms = this.preco * 17 / 100;
+function calcularIcms(aliquota) {
+    this.icms = this.preco * aliquota / 100;
 }
 
+// Neste caso, temos a criação de um método (ação ou atividade) do objeto.
+Produto.prototype.calcularIcms = calcularIcms;
