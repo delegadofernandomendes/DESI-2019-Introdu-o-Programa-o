4f2f9c770e0d38fa8204a6f008dc57d711225e90
delegadofernandomendes <delegadofernandomendes@gmail.com>
2019-12-03 21:31:00 -0300

    Aula 03 Dez 2019 Prévia da Apresentação da SA

---
+++ sa/WebContent/js/ajuda.js
@@ -0,0 +1,23 @@
+function ajuda() {
+    alert("Modo ajuda ativado!");
+}
+
+function ajudaEstado() {
+    alert("O menu CADASTRO DE ESTADOS permite inserir um novo Estado ainda não registrado no sistema. Digite no campo apropriado o nome do Estado que deseja cadastrar; após, clique no botão SALVAR. O nome do país já estará registrado por padrão. Você poderá retornar ao MENU PRINCIPAL clicando no botão VOLTAR ou clicando na logo MACHINE CODE localizada no topo da página.");
+}
+
+function ajudaCidade() {
+    alert("O menu CADASTRO DE CIDADES permite inserir uma nova Cidade ainda não registrada no sistema. Digite no campo apropriado (NOME) o nome da cidade que deseja cadastrar; após, selecione na caixa de opções abaixo o Estado vinculado àquela cidade. Finalizada a operação, clique no botão SALVAR. Você poderá retornar ao MENU PRINCIPAL clicando no botão VOLTAR ou clicando na logo MACHINE CODE localizada no topo da página.");
+}
+
+function ajudaCliente() {
+    alert("O menu CADASTRO DE CLIENTES permite inserir um novo cliente na base de dados da aplicação. Preencha os campos solicitados e, após, clique no botão SALVAR para finalizar o cadastro. Os campos CIDADE e ESTADO poderão ser selecionados dentre as opções cadastradas no sistema. Caso o cliente já esteja cadastrado, o sistema emitirá um alerta informando essa condição. Você poderá retornar ao MENU PRINCIPAL clicando no botão VOLTAR ou clicando na logo MACHINE CODE localizada no topo da página.");
+}
+
+function ajudaPesquisa() {
+    alert("O menu PESQUISA DE CLIENTES permite consultar um cliente já cadastrado na aplicação. A pesquisa poderá ser realizada a partir do CPF, devendo esse dado ser digitado no campo apropriado. Após, clique em PESQUISAR. Caso o cliente digitado esteja cadastrado na base de dados, as informações correspondentes aparecerão na tela de consulta, sendo possível ao usuário realizar a edição do cadastro localizado. Você poderá retornar ao MENU PRINCIPAL clicando no botão VOLTAR ou clicando na logo MACHINE CODE localizada no topo da página.");
+}
+
+function ajudaRelatorio() {
+    alert("O menu RELATÓRIOS permite consultar todos os clientes já cadastrados na aplicação. Ao clicar no link RELATÓRIOS, o sistema exibirá uma lista com os dados na ordem em que foram cadastrados. Você poderá realizar a exclusão de um registro clicando no botão EXCLUIR, localizado ao LADO DIREITO da tela. Para EDITAR o registro, clique no botão EDITAR; após, realize as correções necessárias e clique no botão SALVAR, para concluir a edição. Para cancelar a operação, você poderá clicar no ícone representado com um X, oportunidade em que o sistema retornará ao status anterior, apresentando apenas a lista com os nomes cadastrados. Você poderá retornar ao MENU PRINCIPAL clicando no botão VOLTAR ou clicando na logo MACHINE CODE localizada no topo da página.");
+}
